90d104011ad0a890abcb290bbdc69b3550a9d8c1
mainbosskay <dadakehinde80@yahoo.com>
2024-01-10 07:20:48 -0800

    Creating a function def roman_to_int(roman_string): that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,11 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
-    romandict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romandt = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    indec = [romandt[num] for num in roman_string]
     figure = 0
-    for dig in range(len(roman_string)):
-        if dig > 0 and romandict[roman_string[dig]] > romandict[roman_string[dig - 1]]:
-            figure += romandict[roman_string[dig]] - 2 * \
-                        romandict[roman_string[dig - 1]]
-        else:
-            figure += romandict[roman_string[dig]]
+    for dig in range(len(indec)):
+        figure += indec[digit]
+        if indec[digit - 1] < indec[digit] and digit != 0:
+            figure -= (indec[digit - 1] + indec[digit - 1])
     return figure
