930db5daf16db76ec84deb9ccef82279770cecab
mainbosskay <dadakehinde80@yahoo.com>
2024-02-04 23:03:31 -0800

    Python program that writes a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """Defined basegeometry"""
+
+    def area(self):
+        """Area of a basegeometry that is not implemented"""
+        raise Exception("area() is not implemented")
