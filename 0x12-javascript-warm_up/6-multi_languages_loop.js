75a458079ff1543bc8d8f4ca185505e2973bc6e7
mainbosskay <dadakehinde80@yahoo.com>
2024-04-08 16:15:40 -0700

    JavScript that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -7,3 +7,4 @@ const txts = [
 for (let k = 0; k <txts.length; k++) {
   console.log(txts[k]);
 }
+// that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loopScript 
