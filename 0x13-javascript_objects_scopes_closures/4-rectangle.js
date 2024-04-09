903d16d7f8359e6db419b8f2653da5746f5bf0f1
mainbosskay <dadakehinde80@yahoo.com>
2024-04-09 11:06:23 -0700

    JavaScript that writes a class Rectangle that defines a rectangle: whict creates an instance method called rotate() that exchanges the width and the height of the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let k = 0; k < this.height; k++) {
+      let recgle = '';
+      for (let t = 0; t < this.width; t++) {
+        recgle += 'X';
+      }
+      console.log(recgle);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
