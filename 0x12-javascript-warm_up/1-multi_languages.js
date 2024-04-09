9cea3dd0848b4bec1e5f53093f26a1ba5ab2e0c0
mainbosskay <dadakehinde80@yahoo.com>
2024-04-08 17:30:38 -0700

    JavaScript that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const line1 = 'C is fun';
-const line2 = 'Python is cool';
-const line3 = 'JavaScript is amazing';
-console.log(line1);
-console.log(line2);
-console.log(line3);
+['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(lns => console.log(lns));
