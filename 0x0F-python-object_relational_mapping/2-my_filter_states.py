9397c10cb1ee9c260ab2dad737c3b4b4c045411d
mainbosskay <dadakehinde80@yahoo.com>
2024-04-12 16:10:09 -0700

    Python program that writes a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Argument and display values in states table of hbtn_0e_0_usa where name matches the argument"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 5:
+        dbconnct = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+        )
+        dbcurrt = dbconnct.cursor()
+        statenm = sys.argv[4]
+        dbquery = (
+            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
+            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(statenm)
+        )
+        dbcurrt.execute(dbquery)
+        quryres = dbcurrt.fetchall()
+        for row in quryres:
+            print(row)
+        dbconnct.close()
