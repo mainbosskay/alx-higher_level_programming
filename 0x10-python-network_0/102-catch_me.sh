b03e7b192de4556f9b8d109e8765a7f9eaa4f5e5
mainbosskay <dadakehinde80@yahoo.com>
2024-04-25 07:50:48 -0700

    Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
+curl -s -X PUT -H "Origin: HolbertonSchool" -L --max-redirs -1 -d "user_id=98" "0.0.0.0:5000/catch_me"
