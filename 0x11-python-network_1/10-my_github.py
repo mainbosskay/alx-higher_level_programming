12f740743d439cd647111936129acded89a8c91c
mainbosskay <dadakehinde80@yahoo.com>
2024-04-26 10:23:37 -0700

    Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials (username and password)"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        urldt = "https://api.github.com/user"
+        username = sys.argv[1]
+        password = sys.argv[2]
+        api_headers = {
+                "Accept": "application/vnd.github.v3+json",
+                "Username": username,
+                "Authorization": f"token {password}"
+        }
+        urlresp = requests.get(urldt, headers=api_headers)
+        urlstatus = urlresp.status_code
+        if urlstatus == 200:
+            github_user = urlres.json()
+            if github_user["login"] == username:
+                print(github_user["id"])
+            else:
+                print("None")
+        else:
+            print("None")
