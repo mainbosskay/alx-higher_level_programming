cd1bebb0f7af33ce45855380db9205c5c5ed8c14
mainbosskay <dadakehinde80@yahoo.com>
2024-05-29 06:31:18 -0700

    JavaScript script that updates the text color of the <header> element to red (#FF0000) and must be imported from the <head> tag, not at the end of the HTML

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// JavaScript that updates the text color of the header and must be imported from the <head> tag
+'use strict';
+window.addEventListener('load', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});
